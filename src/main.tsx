937a97c11d7ed64c2ff63145a19924e1c0dedf22
Hashirooy <battlenetruss@gmail.com>
2026-01-02 12:03:29 +0700

    test2

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+
+import { createRoot } from 'react-dom/client'
+import './app/styles/reset.css'
+import App from './app/App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+    <App />
+
+)
